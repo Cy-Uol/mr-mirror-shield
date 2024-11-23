6dfdd386f0be3649bbcde107756433c386197b8a
Cy <28988825@students.lincoln.ac.uk>
2024-11-23 03:49:41 +0000

    last fucking commit of the "night"

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,7 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -13,9 +14,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	if Input.is_action_just_pressed("attack"):
-		animated_sprite.play("default")
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -33,6 +31,8 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+		
+	
 		
 	if direction:
 		velocity.x = direction * SPEED
