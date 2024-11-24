1cbef498bc5a478bb4700f33784d0d0c2eef9ba7
Harrisson06 <28958802@students.lincoln.ac.uk>
2024-11-24 00:20:36 +0000

    Not fuck cy

---
+++ Scenes/kill_zone.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@onready var timer = $Timer
+@onready var game_over = $/root/Tutorial/GameOverScreen
+
+func _on_body_entered(body: Node2D) -> void:
+	print(body.name)
+	if body.get_node("CollisionShape2D") and body.name != "Slime":
+		Engine.time_scale = 0.5
+		body.get_node("CollisionShape2D").queue_free()
+		game_over.game_over_start()
+		Global.dead = true
+		timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	game_over.game_over_end()
+	get_tree().reload_current_scene()
+	
